74ef38ab6bc29e5e8546f73ca3ecd0da66ee4b9b
Mrgreenstone07 <sudhanshusekharbarah@gmail.com>
2026-06-19 18:05:44 +0530

    Added and Updated all fields in Personal details page

--- src/test/java/pages/MyInfoPage.java
+++ src/test/java/pages/MyInfoPage.java
@@ -1,14 +1,18 @@
 package pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
-import org.testng.Assert;
+//import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+//import org.testng.Assert;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class MyInfoPage {
-    
+
     private final WebDriver driver;
 
     private final By myInfoMenu = By.xpath("//span[text()='My Info']");
@@ -16,58 +20,225 @@ public class MyInfoPage {
     private final By pdFirstName = By.xpath("//input[@name = 'firstName']");
     private final By pdMiddleName = By.xpath("//input[@name = 'middleName']");
     private final By pdLastName = By.xpath("//input[@name = 'lastName']");
-    private final By pdEmpId = By.xpath("//label[normalize-space()='Employee Id']/ancestor::div[contains(@class,'oxd-input-group')]//input");
-    private final By pdOtherEmpId = By.xpath("//label[normalize-space()='Other Id']/ancestor::div[contains(@class,'oxd-input-group')]//input");
-    private final By pdDrivingLicenceNmbr = By.xpath("//label[normalize-space()=concat('Driver',\"'\",'s License Number')]/ancestor::div[contains(@class,'oxd-input-group')]//input");
-    private final By pdLicenceExpDate = By.xpath("//label[normalize-space()='License Expiry Date']/ancestor::div[contains(@class,'oxd-input-group')]//input");
-
-
-
+    private final By pdEmpId = By
+            .xpath("//label[normalize-space()='Employee Id']/ancestor::div[contains(@class,'oxd-input-group')]//input");
+    private final By pdOtherEmpId = By
+            .xpath("//label[normalize-space()='Other Id']/ancestor::div[contains(@class,'oxd-input-group')]//input");
+    private final By pdDrivingLicenceNmbr = By.xpath(
+            "//label[normalize-space()=concat('Driver',\"'\",'s License Number')]/ancestor::div[contains(@class,'oxd-input-group')]//input");
+    private final By pdLicenceExpDate = By.xpath(
+            "//label[normalize-space()='License Expiry Date']/ancestor::div[contains(@class,'oxd-input-group')]//input");
+    private final By pdNationalityDrpdwn = By.xpath(
+            "//label[normalize-space()='Nationality']/ancestor::div[contains(@class,'oxd-input-group')]//div[contains(@class,'oxd-select-text--active')]");
+    // private final By pdNationalityDrpdwnOptions =
+    // By.xpath("//div[@role='listbox']//div[@role='option'][normalize-space()='\" +
+    // nationality + \"']')]");
+    private final By pdMaritalStatusDrpdwn = By.xpath(
+            "//label[normalize-space()='Marital Status']/ancestor::div[contains(@class,'oxd-input-group')]//div[contains(@class,'oxd-select-text')]");
+    private final By pdDob = By.xpath(
+            "//label[normalize-space()='Date of Birth']/ancestor::div[contains(@class,'oxd-input-group')]//input");
+    private final By pdGenderMale = By.xpath("//label[normalize-space()='Male']");
+    private final By pdGenderFeMale = By.xpath("//label[normalize-space()='Female']");
+    private final By pdBloodType = By.xpath(
+            "//label[normalize-space()='Blood Type']/ancestor::div[contains(@class,'oxd-input-group')]//div[contains(@class,'oxd-select-text--active')]");
+    private final By pdPersonalSaveBtn = By.xpath("(//button[@type = 'submit'])[1]");
+    private final By pdTestField = By
+            .xpath("//label[normalize-space()='Test_Field']/ancestor::div[contains(@class,'oxd-input-group')]//input");
+    private final By pdCustomSaveBtn = By.xpath("(//button[@type = 'submit'])[2]");
+    private final By pdPhoto = By.xpath("//div[@class = 'orangehrm-edit-employee-image']");
+    private final By menuNavigator3Bars = By.xpath("//div[contains(@class,'oxd-topbar-header-title')]");
+    // private final By ProfilePicturePlusIconbtn = By.xpath("//button[@class =
+    // 'oxd-icon-button oxd-icon-button--solid-main employee-image-action']");
+    private final By profilePhotoUploadInput = By.xpath("//input[@type='file' and contains(@class,'oxd-file-input')]");
+    private final By profilePictureSaveBtn = By.xpath("//button[@type = 'submit']");
+    private final By formLoader = By.xpath("//div[contains(@class,'oxd-form-loader')]");
+    // loacator for verifying success message
+    private final By successToastMessage = By.xpath("//p[contains(@class,'oxd-text--toast-message')]");
 
     public MyInfoPage(WebDriver driver) {
         this.driver = driver;
     }
-    
-    public void clickMyInfoMenu(){
+   
+    public void waitForLoaderToDisappear() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(formLoader));
+    }
+
+    // reusable helper method for inputs
+    private void clearAndType(By locator, String value) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        waitForLoaderToDisappear();
+
+        WebElement field = wait.until(ExpectedConditions.elementToBeClickable(locator));
+        field.click();
+        field.sendKeys(Keys.CONTROL + "a");
+        field.sendKeys(Keys.DELETE);
+        field.sendKeys(value);
+    }
+
+    // helper method for dropdown selection
+    private void selectDropdownOption(By dropdownLocator, String optionText) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        waitForLoaderToDisappear();
+
+        WebElement dropdown = wait.until(ExpectedConditions.elementToBeClickable(dropdownLocator));
+        dropdown.click();
+
+        By optionLocator = By
+                .xpath("//div[@role='listbox']//div[@role='option'][normalize-space()='" + optionText + "']");
+        wait.until(ExpectedConditions.elementToBeClickable(optionLocator)).click();
+    }
+
+    // helper method for radio button click
+    private void clickRadioOption(By radioLocator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        waitForLoaderToDisappear();
+
+        WebElement radio = wait.until(ExpectedConditions.elementToBeClickable(radioLocator));
+        radio.click();
+    }
+
+    public void clickMyInfoMenu() {
         driver.findElement(myInfoMenu).click();
     }
 
-    public boolean myInfoPageIsDisplayed(){
+    public boolean myInfoPageIsDisplayed() {
         return driver.findElement(myInfoHeader).isDisplayed();
     }
 
-    public void pdFillEmployeeName(String firstName,String middleName, String lastName) throws InterruptedException{
-         driver.findElement(pdFirstName).sendKeys(Keys.CONTROL + "a");
-         driver.findElement(pdFirstName).sendKeys(Keys.DELETE);
-         driver.findElement(pdFirstName).sendKeys(firstName);
-         driver.findElement(pdMiddleName).sendKeys(Keys.CONTROL + "a");
-         driver.findElement(pdMiddleName).sendKeys(Keys.DELETE);
-         driver.findElement(pdMiddleName).sendKeys(middleName);
-         driver.findElement(pdLastName).sendKeys(Keys.CONTROL + "a");
-         driver.findElement(pdLastName).sendKeys(Keys.DELETE);
-         driver.findElement(pdLastName).sendKeys(lastName);
-
+    public void editProfilePicture() {
+        driver.findElement(pdPhoto).click();
     }
 
-    public void fillEmpIdAndOtherId(String EmpId,String OtherId){
-        driver.findElement(pdEmpId).sendKeys(Keys.CONTROL + "a");
-         driver.findElement(pdEmpId).sendKeys(Keys.DELETE);
-         driver.findElement(pdEmpId).sendKeys(EmpId);
+    public void uploadProfilePicture(String fileName) {
+        String filePath = System.getProperty("user.dir") + "\\src\\test\\resources\\images\\" + fileName;
 
-         driver.findElement(pdOtherEmpId).sendKeys(Keys.CONTROL + "a");
-         driver.findElement(pdOtherEmpId).sendKeys(Keys.DELETE);
-         driver.findElement(pdOtherEmpId).sendKeys(OtherId);
+        // driver.findElement(ProfilePicturePlusIconbtn).click();
+        driver.findElement(profilePhotoUploadInput).sendKeys(filePath);
     }
 
-    public void fillDrivingLicenceNumAndExpDate(String Dl,String DlExpDate){
-        driver.findElement(pdDrivingLicenceNmbr).sendKeys(Keys.CONTROL + "a");
-        driver.findElement(pdDrivingLicenceNmbr).sendKeys(Keys.DELETE);
-        driver.findElement(pdDrivingLicenceNmbr).sendKeys(Dl);
-
-        driver.findElement(pdLicenceExpDate).sendKeys(Keys.CONTROL + "a");
-        driver.findElement(pdLicenceExpDate).sendKeys(Keys.DELETE);
-        driver.findElement(pdLicenceExpDate).sendKeys(DlExpDate);
+    public void saveProfilePicture() {
+        driver.findElement(profilePictureSaveBtn).click();
     }
 
-    
+    public void menuNavigator() {
+        driver.findElement(menuNavigator3Bars).click();
+    }
+
+    public void pdFillEmployeeName(String firstName, String middleName, String lastName) {
+        clearAndType(pdFirstName, firstName);
+        clearAndType(pdMiddleName, middleName);
+        clearAndType(pdLastName, lastName);
+    }
+
+    public void fillEmpIdAndOtherId(String empId, String otherId) {
+        clearAndType(pdEmpId, empId);
+        clearAndType(pdOtherEmpId, otherId);
+    }
+
+    public void fillDrivingLicenceNumAndExpDate(String dl, String dlExpDate) {
+        clearAndType(pdDrivingLicenceNmbr, dl);
+        clearAndType(pdLicenceExpDate, dlExpDate);
+    }
+
+    public void selectNationality(String nationality) {
+        selectDropdownOption(pdNationalityDrpdwn, nationality);
+    }
+
+    public void selectMaritalStatus(String maritalStatus) {
+        selectDropdownOption(pdMaritalStatusDrpdwn, maritalStatus);
+    }
+
+    public void selectDob(String dob) {
+        clearAndType(pdDob, dob);
+    }
+
+    public void selectGenderMale() {
+        clickRadioOption(pdGenderMale);
+    }
+
+    public void selectGenderFeMale() {
+        clickRadioOption(pdGenderFeMale);
+    }
+
+    public void clickPersonalDetailsSavebtn() {
+        driver.findElement(pdPersonalSaveBtn).click();
+    }
+
+    public void selectBloodType(String bloodType) {
+        selectDropdownOption(pdBloodType, bloodType);
+    }
+
+    public void fillTestField(String testField) {
+        clearAndType(pdTestField, testField);
+    }
+
+    public void clickCustomSaveBtn() {
+        driver.findElement(pdCustomSaveBtn).click();
+    }
+
+    // Verify text input fields
+    public String getFirstNameValue() {
+        return driver.findElement(pdFirstName).getAttribute("value");
+    }
+
+    public String getMiddleNameValue() {
+        return driver.findElement(pdMiddleName).getAttribute("value");
+    }
+
+    public String getLastNameValue() {
+        return driver.findElement(pdLastName).getAttribute("value");
+    }
+
+    public String getEmployeeIdValue() {
+        return driver.findElement(pdEmpId).getAttribute("value");
+    }
+
+    public String getOtherIdValue() {
+        return driver.findElement(pdOtherEmpId).getAttribute("value");
+    }
+
+    public String getDrivingLicenceValue() {
+        return driver.findElement(pdDrivingLicenceNmbr).getAttribute("value");
+    }
+
+    public String getLicenceExpiryDateValue() {
+        return driver.findElement(pdLicenceExpDate).getAttribute("value");
+    }
+
+    public String getDobValue() {
+        return driver.findElement(pdDob).getAttribute("value");
+    }
+
+    public String getTestFieldValue() {
+        return driver.findElement(pdTestField).getAttribute("value");
+    }
+
+    // Verify selected dropdown values
+    public String getSelectedNationality() {
+        return driver.findElement(pdNationalityDrpdwn).getText().trim();
+    }
+
+    public String getSelectedMaritalStatus() {
+        return driver.findElement(pdMaritalStatusDrpdwn).getText().trim();
+    }
+
+    public String getSelectedBloodType() {
+        return driver.findElement(pdBloodType).getText().trim();
+    }
+
+    // Verify selected gender
+    public boolean isMaleSelected() {
+        return driver.findElement(pdGenderMale).getAttribute("class").contains("--checked")
+                || driver.findElement(pdGenderMale).getText().contains("Male");
+    }
+
+    // Verify success message
+    public String getSuccessToastMessage() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(successToastMessage)).getText().trim();
+    }
 }
--- src/test/java/tests/MyInfoTest.java
+++ src/test/java/tests/MyInfoTest.java
@@ -2,16 +2,18 @@ package tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import base.BaseTest;
 import pages.DashboardPage;
 import pages.LoginPage;
 import pages.MyInfoPage;
 
-public class MyInfoTest extends BaseTest{
-    
+public class MyInfoTest extends BaseTest {
+
     @Test
-    public void adminPageShouldOpenAfterLogin() throws InterruptedException {
+    public void adminPageShouldOpenAfterLogin()  {
         LoginPage loginPage = new LoginPage(driver);
         DashboardPage dashboardPage = new DashboardPage(driver);
         MyInfoPage myInfoPage = new MyInfoPage(driver);
@@ -22,30 +24,91 @@ public class MyInfoTest extends BaseTest{
 
         String EmpId = "Sid";
         String OtherId = "E5858";
-        
-        String Dl = "D78787";
-        String DlExpDate = "2026-18-08";
 
-         loginPage.open();
+        String Dl = "D78787";
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-dd-MM");
+
+        LocalDate futureLicenseDate = LocalDate.now().plusYears(2);
+        String dlExpDate = futureLicenseDate.format(formatter);
+
+
+        LocalDate dob = LocalDate.now().minusYears(25);
+        String dobDate = dob.format(formatter);
+
+        // String DlExpDate = "2026-18-08";
+        // String Dob = "1999-08-08";
+
+        String Nationality = "Indian";
+
+        String maritalStatus = "Single";
+
+        String bloodType = "B+";
+
+        String testField = "888";
+
+        loginPage.open();
         loginPage.login(validUsername, validPassword);
 
         Assert.assertTrue(dashboardPage.isDashboardVisible(), "Dashboard is not visible after login.");
 
         myInfoPage.clickMyInfoMenu();
-         
-        Thread.sleep(5000);
 
-        Assert.assertTrue(myInfoPage.myInfoPageIsDisplayed(),"My Info page is visible on clicking");
-        
-        Thread.sleep(5000);
-        myInfoPage.pdFillEmployeeName(firstName,middleName,lastName);
+        //Thread.sleep(5000);
+
+        Assert.assertTrue(myInfoPage.myInfoPageIsDisplayed(), "My Info page is visible on clicking");
+
+        // Thread.sleep(5000);
+
+        myInfoPage.editProfilePicture();
+
+        myInfoPage.uploadProfilePicture("profilePicture.png");
+        myInfoPage.saveProfilePicture();
+        myInfoPage.menuNavigator();
+        myInfoPage.clickMyInfoMenu();
+
+        myInfoPage.pdFillEmployeeName(firstName, middleName, lastName);
+        Assert.assertEquals(myInfoPage.getFirstNameValue(), firstName, "First name did not update correctly.");
+        Assert.assertEquals(myInfoPage.getMiddleNameValue(), middleName, "Middle name did not update correctly.");
+        Assert.assertEquals(myInfoPage.getLastNameValue(), lastName, "Last name did not update correctly.");
 
         myInfoPage.fillEmpIdAndOtherId(EmpId, OtherId);
+        Assert.assertEquals(myInfoPage.getEmployeeIdValue(), EmpId, "Employee Id did not update correctly.");
+        Assert.assertEquals(myInfoPage.getOtherIdValue(), OtherId, "Other Id did not update correctly.");
 
-        myInfoPage.fillDrivingLicenceNumAndExpDate(Dl,DlExpDate);
+        myInfoPage.fillDrivingLicenceNumAndExpDate(Dl, dlExpDate);
+        Assert.assertEquals(myInfoPage.getDrivingLicenceValue(), Dl,
+                "Driving License Number did not update correctly.");
+        Assert.assertEquals(myInfoPage.getLicenceExpiryDateValue(), dlExpDate,
+                "License Expiry Date did not update correctly.");
 
+        myInfoPage.selectNationality(Nationality);
+        Assert.assertEquals(myInfoPage.getSelectedNationality(), Nationality, "Nationality did not update correctly.");
 
+        myInfoPage.selectMaritalStatus(maritalStatus);
+        Assert.assertEquals(myInfoPage.getSelectedMaritalStatus(), maritalStatus,
+                "Marital Status did not update correctly.");
 
+        myInfoPage.selectDob(dobDate);
+        Assert.assertEquals(myInfoPage.getDobValue(), dobDate, "Date of Birth did not update correctly.");
+
+        myInfoPage.selectGenderMale();
+        Assert.assertTrue(myInfoPage.isMaleSelected(), "Male gender option was not selected.");
+        
+        //Capture screenshot before save
+        captureScreenshot("personal_details_before_save");
+
+        myInfoPage.clickPersonalDetailsSavebtn();
+        Assert.assertEquals(myInfoPage.getSuccessToastMessage(), "Successfully Updated", "Personal details save toast not shown.");
+
+        myInfoPage.selectBloodType(bloodType);
+        Assert.assertEquals(myInfoPage.getSelectedBloodType(), bloodType, "Blood Type did not update correctly.");
+        myInfoPage.fillTestField(testField);
+        Assert.assertEquals(myInfoPage.getTestFieldValue(), testField, "Test_Field did not update correctly.");
+        // Capture screenshot before save
+        captureScreenshot("custom_fields_before_save");
+        myInfoPage.clickCustomSaveBtn();
+        Assert.assertEquals(myInfoPage.getSuccessToastMessage(), "Successfully Saved", "Custom fields save toast not shown.");
 
     }
 }
